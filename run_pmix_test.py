8a183cd9af005f090db58d4eb93232aa43af445a
Niccolo Tosato <niccolo.tosato@yahoo.it>
2026-04-14 13:30:48 -0400

    Get runtime from tests

--- run_pmix_test.py
+++ run_pmix_test.py
@@ -2,7 +2,7 @@ import os
 import reframe as rfm
 import reframe.utility.typecheck as typ
 import reframe.utility.sanity as sn
-
+import time
 from prrte_build_class import build_prrte
 from pmix_build_class import build_pmix
 from libevent_build_class import build_libevent
@@ -41,8 +41,22 @@ class base_test(rfm.RunOnlyRegressionTest):
         self.executable = os.path.join("")
         # prepare the environment, with LD and PATH
     @sanity_function
-    def dummy(self):
+    def retcode(self):
         return sn.assert_eq(self.job.exitcode,0)
+    @run_before('run', always_last=True)
+    def start_internal_timer(self):
+        # Starts the moment ReFrame prepares to submit the job to Slurm
+        self.t_start = time.time()
+
+    @run_after('run')
+    def stop_internal_timer(self):
+        # Triggers the moment ReFrame detects the Slurm job has finished
+        self.t_end = time.time()
+
+    @performance_function('s')
+    def total_walltime(self):
+        # Calculate the difference and return it as your performance metric
+        return self.t_end - self.t_start
 
 @rfm.simple_test
 class hello_test(base_test):
@@ -58,7 +72,28 @@ class hello_test(base_test):
         print(test_path)
         self.prerun_cmds = [ f'cd {test_path}', './build.sh' ]    
         self.executable="./run.sh"
+    @performance_function('s')
+    def hostname_test(self):
+        patt = r"^RUNTIME,(\d+\.\d+),(\d+\.\d+),(\d+\.\d+)"
+        # Extract the values
+        return sn.extractall(
+            patt, 
+            self.stderr,          
+            tag=(1),        # Capture Group 1 (Real), Group 2 (User), Group 3 (Sys), Get only 1
+            conv=float            
+        )[0]
+    @performance_function('s')
+    def pmix_lib_test(self):
+        patt = r"^RUNTIME,(\d+\.\d+),(\d+\.\d+),(\d+\.\d+)"
+        # Extract the values
+        return sn.extractall(
+            patt, 
+            self.stderr,          
+            tag=(1),        # Capture Group 1 (Real), Group 2 (User), Group 3 (Sys), Get only 1
+            conv=float            
+        )[1]
 
+    
 @rfm.simple_test
 class cycle_test(base_test):
     descr = "Test Cycle in pmix-test"
@@ -90,6 +125,7 @@ class prun_wrapper_test(base_test):
     def compile_test(self):
         test_dir_base = self.pmix_tests.stagedir
         test_path = os.path.join(test_dir_base, "pmix-tests/prrte", self.test_name)
+        print(test_path)
         self.prerun_cmds = [ f'cd {test_path}', './build.sh', 'scontrol show hostnames $SLURM_JOB_NODELIST > hostfile.txt' ]    
         self.executable="./run.sh"
         self.env_vars['CI_HOSTFILE'] = f"{os.path.join(test_path,'hostfile.txt')}"
