5ceb6db647ed6fb0448f0acc3a891187020c53fd
Niccolo Tosato <niccolo.tosato@yahoo.it>
2026-04-14 10:07:36 -0400

    Generate an hostfile to run prterun wrapper test

--- run_pmix_test.py
+++ run_pmix_test.py
@@ -9,7 +9,7 @@ from libevent_build_class import build_libevent
 
 class fetch_pmixtest(rfm.RunOnlyRegressionTest):
     descr = "Fetch pmix test"
-    repository = f"https://github.com/openpmix/pmix-tests.git"
+    repository = f"https://github.com/NiccoloTosato/pmix-tests.git"
     executable = 'git'
     executable_opts = ["clone",f"{repository}"]
     local = True
@@ -73,6 +73,7 @@ class cycle_test(base_test):
         print(test_path)
         self.prerun_cmds = [ f'cd {test_path}', './build.sh' ]    
         self.executable="./run.sh"
+
 @rfm.simple_test
 class prun_wrapper_test(base_test):
     descr = "Test prun-wrapper in pmix-test"
@@ -80,11 +81,16 @@ class prun_wrapper_test(base_test):
     num_tasks = 120
     num_tasks_per_node = 12
 
+    @run_after("init")
+    def setup_deps(self):
+        # This is encessary, there could be concurrency problems, since both test compile hello world
+        self.depends_on('hello_test')
+        
     @run_before("run")
     def compile_test(self):
         test_dir_base = self.pmix_tests.stagedir
         test_path = os.path.join(test_dir_base, "pmix-tests/prrte", self.test_name)
         print(test_path)
-        self.prerun_cmds = [ f'cd {test_path}', './build.sh' ]    
+        self.prerun_cmds = [ f'cd {test_path}', './build.sh', 'scontrol show hostnames $SLURM_JOB_NODELIST > hostfile.txt' ]    
         self.executable="./run.sh"
-    
+        self.env_vars['CI_HOSTFILE'] = f"{os.path.join(test_path,'hostfile.txt')}"
